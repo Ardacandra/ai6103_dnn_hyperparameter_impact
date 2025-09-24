3ae8b808e35ac0363b0924d36ac5fd316e9ee336
Ardacandra <ardacandrasubiantoro@gmail.com>
2025-09-24 13:00:12 +0800

    add custom batch normalization

--- src/model.py
+++ src/model.py
@@ -10,6 +10,58 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
+class CustomBatchNorm(nn.Module):
+    def __init__(self, num_features, eps=1e-5, alpha=0.9):
+        """
+        Custom class to implement batch normalization 
+        where the channel-wise mean and variance does not participate in gradient calculation.
+
+        Normalization will be done over the channel dimension "C"
+        from a 4 dimensional input with size (N, C, H, W).
+
+        Args:
+            num_features (int): channel count (C) from input (N, C, H, W)
+            eps (float): small constant to avoid division by zero
+            alpha (float): decay factor running mean and variance update
+
+        Shape:
+            - Input : (N, C, H, W)
+            - Output : (N, C, H, W)
+
+        """
+        super().__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.alpha = alpha
+
+        #initialize learnable parameters
+        self.weight = nn.Parameter(torch.ones(1, self.num_features, 1, 1))
+        self.bias = nn.Parameter(torch.zeros(1, self.num_features, 1, 1))
+
+        #initialize running stats for inference
+        #use register_buffer to make it persistent but not trainable
+        self.register_buffer("running_mean", torch.zeros(1, num_features, 1, 1))
+        self.register_buffer("running_var", torch.ones(1, num_features, 1, 1))
+
+    def forward(self, x):
+        if self.training:
+            #compute the mean and variance per channel
+            #detach x to prevent gradient flow
+            mean = x.detach().mean(dim=(0, 2, 3), keepdim=True)
+            var = x.detach().var(dim=(0, 2, 3), unbiased=False, keepdim=True) #unbiased=False -> divide by NHW, not NHW-1
+
+            #update running statistics
+            self.running_mean.mul_(self.alpha).add_((1 - self.alpha) * mean)
+            self.running_var.mul_(self.alpha).add_((1 - self.alpha) * var)
+        else:
+            #use running stats for inference
+            mean = self.running_mean
+            var = self.running_var
+
+        #apply batch normalization
+        out = self.weight * (x - mean) / torch.sqrt(var + self.eps) + self.bias
+        return out
+
 # defining resnet models
 
 class BasicBlock(nn.Module):
@@ -19,17 +71,17 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = CustomBatchNorm(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = CustomBatchNorm(planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                CustomBatchNorm(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -46,20 +98,20 @@ class Bottleneck(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(Bottleneck, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
+        self.bn1 = CustomBatchNorm(planes)
         self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
                                stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
+        self.bn2 = CustomBatchNorm(planes)
         self.conv3 = nn.Conv2d(planes, self.expansion *
                                planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+        self.bn3 = CustomBatchNorm(self.expansion*planes)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_planes != self.expansion*planes:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_planes, self.expansion*planes,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*planes)
+                CustomBatchNorm(self.expansion*planes)
             )
 
     def forward(self, x):
@@ -81,7 +133,7 @@ class ResNet(nn.Module):
         # It should downsample for ImageNet
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.bn1 = CustomBatchNorm(64)
         # four stages with three downsampling
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
